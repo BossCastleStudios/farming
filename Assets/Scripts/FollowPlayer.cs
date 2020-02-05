26cc3b51ac7e79f902cf928d56ba0390c4d12a40
Panagiotis Peikidis <pekalicious@gmail.com>
2020-02-05 03:41:02 -0500

    Simple script to follow player around

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    void FixedUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+}
