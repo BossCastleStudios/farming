a321bb5a30805cf09f47643a04c443a58433da0f
Panagiotis Peikidis <pekalicious@gmail.com>
2020-02-05 02:37:41 -0500

    Added simple hero model, a basic scene, and a simple character movement script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody _rigidbody;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        var gamepad = Gamepad.current;
+        if (gamepad == null)
+            return;
+
+        var gamepadLeftStick = gamepad.leftStick.ReadValue();
+        Vector3 targetVelocity = new Vector3(gamepadLeftStick.x, 0, gamepadLeftStick.y);
+        targetVelocity *= speed;
+
+        transform.position += targetVelocity;
+
+        Vector3 direction = new Vector3(gamepadLeftStick.x, 0, gamepadLeftStick.y);
+        if (direction.magnitude > 0.01F)
+        {
+            direction = Camera.main.transform.TransformDirection(direction);
+            direction.y = 0;
+            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        }
+    }
+}
