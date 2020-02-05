276822b8674a9fdd05126f94939d90703411e27e
Panagiotis Peikidis <pekalicious@gmail.com>
2020-02-05 18:30:26 -0500

    Added Wood amount display

---
+++ Assets/DisplayResource.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayResource : MonoBehaviour
+{
+    public PlayerController player;
+    public TMPro.TextMeshProUGUI wood;
+
+    void Update()
+    {
+        wood.text = $"Wood: {player.WoodResource}";
+    }
+}
