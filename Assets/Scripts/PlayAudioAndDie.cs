c9a8c5fb789a9a9f9aa4a990979fc1cba138c63a
Panagiotis Peikidis <pekalicious@gmail.com>
2020-02-05 19:43:38 -0500

    Added treefall effect

---
+++ Assets/Scripts/PlayAudioAndDie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioAndDie : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(PlayAudioAndDieCoroutine());
+    }
+
+    private IEnumerator PlayAudioAndDieCoroutine()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+        while (audio.isPlaying)
+        {
+            yield return null;
+        }
+        Destroy(this.gameObject);
+    }
+
+}
--- Assets/Scripts/TreeGatherEffect.cs
+++ Assets/Scripts/TreeGatherEffect.cs
@@ -1,22 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class TreeGatherEffect : MonoBehaviour, IGatherEffect
 {
     public GameObject leaves;
     public GameObject trunk;
     public ParticleSystem particles;
+    public AudioSource chopping;
+    public GameObject treeFall;
+
+    private bool instantiatedTreeFall = false;
 
     public void StartGathering()
     {
         particles.Play();
+        chopping.Play();
     }
 
     public void OnGather(float newPercent)
     {
-        if (newPercent < 0.3)
+        if (newPercent < 0.33)
         {
             trunk.transform.localScale = new Vector3(1, 1, 0.5f);
             trunk.transform.localPosition = new Vector3(0, 0.25f, 0);
+            if (!instantiatedTreeFall)
+            {
+                Instantiate(treeFall);
+                instantiatedTreeFall = true;
+            }
+            particles.Stop();
         }
         else if (newPercent < 0.5)
         {
